84be892ad0b914a64a4aa38d0f615985d78c11f4
unknown <ttyn4519@outlook.jp>
2019-05-22 16:58:58 +0900

    improve password_system.py

---
+++ password_system_improve.py
@@ -0,0 +1,10 @@
+password=1887 #好きなパスワードを設定。
+a=int(input("パスワードを入力してください(数字):"))
+c=0
+while password!=a and c<=4: #最初に1回パスワードを入力しているため(1+4=5)
+    a=int(input("正しいパスワードを入力してください:"))
+    c=c+1
+if password==a:
+    print("施錠しました")
+else:
+    print("5回入力ミスをしたため終了します")
